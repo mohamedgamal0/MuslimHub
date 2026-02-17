ce5180094bb88e4fa211854e83b6a3dbfdec37fe
Mohamed Gamal <mohamed.gamal@areebgroup.com>
2026-02-17 14:23:02 +0200

    Initial Commit

---
+++ MuslimHub/MuslimHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuslimHubApp.swift
+//  MuslimHub
+//
+//  Created by Mohamed Gamal on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MuslimHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
